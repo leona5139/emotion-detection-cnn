9af66174b9afc055d8edff8ed1487a2dcb5e002e
alesha-uwu <leona.pierce@proton.me>
2026-01-15 15:18:16 -0500

    initial commit

---
+++ emotion_detection.py
@@ -0,0 +1,44 @@
+# Define CNN Model
+class CNN(nn.Module):
+    def __init__(self, num_classes=4):
+        super().__init__()
+
+        self.features = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(),
+            nn.MaxPool2d(2), 
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(2), 
+
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(128 * 6 * 6, 256),
+            nn.ReLU(),
+            nn.Dropout(0.4),
+            nn.Linear(256, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.classifier(x)
+        return x
+
+# 1. Define the class (you must have the CNN class code in your script)
+model = CNN(num_classes=4)
+
+# 2. Load the weights
+model.load_state_dict(torch.load('cnn_weights.pth'))
+
+# 3. Set to evaluation mode if you are doing inference
+model.eval()
